83f7c17194eb15f6c86240db9a7cadb730181427
iamxiarui <iamxiarui@foxmail.com>
2016-05-08 13:17:17 +0800

    添加平滑动画与开关事件

--- Android_DrawerDemo/src/xr/drawerdemo/MainActivity.java
+++ Android_DrawerDemo/src/xr/drawerdemo/MainActivity.java
@@ -2,17 +2,33 @@ package xr.drawerdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ImageButton;
+import xr.drawerdemo.view.SlideMenuView;
 
 public class MainActivity extends Activity {
+	private SlideMenuView smView;
+	private ImageButton backButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+
+		smView = (SlideMenuView) findViewById(R.id.smv);
+		backButton = (ImageButton) findViewById(R.id.ib_back);
+
+		// 添加按钮点击事件
+		backButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				smView.switchState();
+			}
+		});
 	}
 
 }
--- Android_DrawerDemo/src/xr/drawerdemo/view/SlideMenuView.java
+++ Android_DrawerDemo/src/xr/drawerdemo/view/SlideMenuView.java
@@ -6,6 +6,7 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 
 /**
  * @ClassName: SlideMenuView
@@ -19,23 +20,40 @@ public class SlideMenuView extends ViewGroup {
 	private float moveEndX;
 	private int scrollX;
 
+	private static final int MAIN_MODE = 0;
+	private static final int MENU_MODE = 1;
+	private int currentMode = MAIN_MODE;
+
+	private Scroller scroller;
+
 	// 必须实现其构造函数
 	public SlideMenuView(Context context) {
 		super(context);
+		initView();
 	}
 
 	public SlideMenuView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		initView();
 	}
 
 	public SlideMenuView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
-		// TODO Auto-generated constructor stub
+		initView();
 	}
 
 	@SuppressLint("NewApi")
 	public SlideMenuView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
 		super(context, attrs, defStyleAttr, defStyleRes);
+		initView();
+	}
+
+	/**
+	 * @Title: initVie初始化View
+	 * @return: void
+	 */
+	private void initView() {
+		scroller = new Scroller(getContext());
 	}
 
 	/**
@@ -127,15 +145,83 @@ public class SlideMenuView extends ViewGroup {
 
 			// 如果滑动的距离大于菜单的一半 直接显示菜单
 			if (getScrollX() < leftCenter) {
+				currentMode = MAIN_MODE;
+				showMenuView();
 				scrollTo(-getChildAt(0).getMeasuredWidth(), 0);
 			} else {
 				// 否则返回主菜单
-				scrollTo(0, 0);
+				currentMode = MENU_MODE;
+				showMenuView();
 			}
 
 			break;
 		}
 		return true;
+	}
+
+	/**
+	 * @Title: showMenuView
+	 * @Description:根据滑动距离显示菜单
+	 * @return: void
+	 */
+	private void showMenuView() {
+
+		int startX = getScrollX();
+		// 将要发生水平的变化量
+		int distanceX = 0;
+
+		if (currentMode == MAIN_MODE) {
+			// 水平变化量为主界面移动的距离
+			distanceX = 0 - startX;
+		} else if (currentMode == MENU_MODE) {
+			// 水平变化量为菜单界面还没有显示的距离
+			distanceX = -getChildAt(0).getMeasuredWidth() - startX;
+		}
+
+		// 平滑数据的模拟
+		int duration = Math.abs(distanceX * 2);
+		// 开始平滑
+		scroller.startScroll(startX, 0, distanceX, 0, duration);
+		// 重绘界面
+		invalidate();
+
+	}
+
+	/**
+	 * @Title: computeScroll
+	 * @Description:不断获取模拟数据维持动画的继续
+	 */
+	@Override
+	public void computeScroll() {
+		super.computeScroll();
+		if (scroller.computeScrollOffset()) { // 直到duration事件以后, 结束
+			// true, 动画还没有结束
+			// 获取当前模拟的数据, 也就是要滚动到的位置
+			int currX = scroller.getCurrX();
+			// 直接滑动过去
+			scrollTo(currX, 0);
+			// 重绘界面
+			invalidate();
+		}
+	}
+
+	/**
+	 * @Title: switchState
+	 * @Description:根据状态 执行点击事件
+	 * @return: void
+	 */
+	public void switchState() {
+		if (currentMode == MAIN_MODE) {
+			currentMode = MENU_MODE;
+			showMenuView();
+		} else {
+			currentMode = MAIN_MODE;
+			showMenuView();
+		}
+	}
+
+	public int getCurrentState() {
+		return currentMode;
 	}
 
 }
